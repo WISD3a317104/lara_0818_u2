9f0999aaf6024040e10a878b3aafe34e99655b07
s3a317104 <31909567+s3a317104@users.noreply.github.com>
2017-11-28 22:15:20 +0800

    2.1 2.b 2.2

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>@yield('title')|MyBlog</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
