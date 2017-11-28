d3a0c0b6c80ebb1617a4a46647b53e3b00d5f477
s3a317104 <31909567+s3a317104@users.noreply.github.com>
2017-11-28 22:16:51 +0800

    2.3 3.1 3.2

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('title','Hello')
+
+@section('content')
+    <h1>Thisis{{$name}}'shome.</h1>
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
         <title>@yield('title')|MyBlog</title>
     </head>
     <body>
+        @include('layouts.partials.navigation')
         <div class="container">
             @yield('content')
         </div>
